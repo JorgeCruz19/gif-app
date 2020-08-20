6fea536fe11e69fe6f3cccff5a0354fe42a8f9d8
JorgeCruz19 <jcruz@DESKTOP-L3R68CG.localdomain>
2020-08-19 18:39:54 -0600

    add docs

---
+++ docs/precache-manifest.f220a957ca6386d93d3e109f89f7f8a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e06354ef6c146114e20c78a6bc0c6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc328f8a4655e3530300",
+    "url": "/static/css/main.d896ed38.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc328f8a4655e3530300",
+    "url": "/static/js/main.0d817864.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
